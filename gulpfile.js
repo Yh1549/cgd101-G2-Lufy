5f30f4661d3e2bd319dc814c72a951c72bdf73c4
Yh1549 <cpeter618@gmail.com>
2022-05-26 10:31:17 +0800

    0526 翊宏 1.新增 autofixer 壓圖 babel

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const { src, dest, watch, series, parallel } = require('gulp');
 
 // html package
 const fileinclude = require('gulp-file-include');
-
 function includeHTML() {
     return src('src/*.html') // 來源
         .pipe(fileinclude({
@@ -15,23 +14,47 @@ function includeHTML() {
 
 exports.html = includeHTML;
 
+
 // js move
+const babel = require('gulp-babel');
 function moveJs() {
-    return src('src/js/*.js').pipe(dest('dist/js'))
+    return src('src/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dist/js'))
 }
+
 
 //img move
 function moveImg() {
     return src('src/images/*.*').pipe(dest('dist/images'))
 }
 
+
 //productpages move
 function moveProductPages() {
     return src('src/productpages/*.*').pipe(dest('dist/productpages'))
 }
 
+
+// 壓圖
+const imagemin = require('gulp-imagemin');
+function min_images() {
+    return src('src/images/*.*')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 80, progressive: true }) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
+        ]))
+        .pipe(dest('dist/images'))
+}
+
+exports.mini_img = min_images
+
+
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 // sass ->css
 function styleSass() {
@@ -39,6 +62,9 @@ function styleSass() {
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest('./dist/css'));
 }
 
@@ -52,11 +78,10 @@ function watchfile() {
     watch(['./src/sass/*.scss', './src/sass/**/*.scss'], styleSass) // 監看sass
 }
 
+
 // 瀏覽器同步
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
-
-
 function browser(done) {
     browserSync.init({
         server: {
@@ -76,6 +101,7 @@ function browser(done) {
 
 // 監看
 exports.w = series(parallel(moveJs, moveImg, includeHTML, moveProductPages, styleSass), watchfile)
+
 
 //瀏覽器同步
 exports.default = series(parallel(moveJs, includeHTML, styleSass, moveProductPages, moveImg), browser)
