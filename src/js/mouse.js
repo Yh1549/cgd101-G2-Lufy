580813c2187146f30bd2073684977af5d71d9c6e
Yh1549 <cpeter618@gmail.com>
2022-05-30 16:39:23 +0800

    好累

--- src/js/mouse.js
+++ src/js/mouse.js
@@ -1,23 +1,34 @@
 function mouseTracking(e) {
-  let mouse = document.querySelector("#mouse");
-//   console.log(MouseEvent.screenX);
-  mouse.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-        };
+  let mouse = document.querySelector(".mouse");
+  // console.log(MouseEvent.screenX);
+  mouse.style="top: " + (e.pageY - 25) + "px; left: " + (e.pageX - 25) + "px;";
+};
 
-function mouseBig(){
-    let x = 1;
-    console.log(x);
-    let mouse = document.querySelector("#mouse");
-    mouse.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;","scale:2")
-        };
+function mouseBig(e) {
+  // let x = 1;
+  // console.log(x);
+  let mouse = document.querySelector(".mouse");
+  mouse.classList.add('mouseClickani');
+};
 
-
+function mouseReset() {
+  let mouse = document.querySelector(".mouse");
+  mouse.classList.remove('mouseClickani');
+};
+function mouseScale() {
+  let mouse = document.querySelector(".mouse");
+  mouse.classList.remove('mouseClickani');
+  mouse.classList.add('expand');
+}
 function init() {
-    let mouseOn = document.querySelectorAll('.mouseon');
+  let mouseOn = document.querySelectorAll('.mouseOn');
   window.addEventListener("mousemove", mouseTracking, false);
-  for(let i=0; i<mouse.length;i++){
-    mouseOn[i].addEventListener("mouseover",mouseBig, false);
+  for (let i = 0; i < mouseOn.length; i++) {
+    mouseOn[i].addEventListener("mouseover", mouseBig, false);
+    mouseOn[i].addEventListener("mouseout", mouseReset, false);
+    mouseOn[i].addEventListener("click", mouseScale, false);
   }
-//   console.log(mouseOn[0]);
+  // console.log(mouseOn[0]);
+
 }
 window.addEventListener("load", init, false);
