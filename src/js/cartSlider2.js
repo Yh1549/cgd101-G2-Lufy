3df879e087719438b935ce9cdfb1b39dee5faa9b
michael17 <missing821017@yahoo.com.tw>
2022-06-05 15:03:17 +0800

    新購物車新增動態生成結構 加總功能

--- src/js/cartSlider2.js
+++ src/js/cartSlider2.js
@@ -1,19 +1,37 @@
+const { compileString } = require("sass");
+
 function showCart() {
 
     let cartv2 = document.querySelector(".shopping_cart_box");
 
     cartv2.classList.toggle('hidden');
 
+    let itemString = localStorage.getItem('addItemList')
+    let items = itemString.substr(0, itemString.length - 2).split(', ')
 
-    showItem();
+
+
+    let total = 0
+
+    for (let i = 0; i < items.length; i++) {
+        let itemInfo = localStorage.getItem(items[i])
+        showItem(items[i], itemInfo)
+
+        let itemPrice = parseInt(itemInfo.split('|')[2])
+        total += itemPrice
+        console.log(total);
+    }
+
+    document.getElementById('cart-total').innerText = total
+
 
 }
 
 
 
-function showItem() {
+function showItem(itemId, Value) {
 
-    let Value = localStorage.getItem(localStorage.key(1))
+    Value = localStorage.getItem(itemId)
 
     let image = document.createElement('img')
     image.src = 'images/' + Value.split('|')[1]
@@ -24,18 +42,27 @@ function showItem() {
     let price = document.createElement('span')
     price.innerText = Value.split('|')[2]
 
-    let product = document.querySelector('.shopping-cart .product')
+    // let product = document.querySelector('.shopping-cart .product')
 
-    if (product.hasChildNodes()) {
-        while (product.childNodes.length >= 1) {
-            product.removeChild(product.firstChild)
-        }
-    }
+    let shoppingCart = document.querySelector('.shopping-cart')
+
+
+
+    let product = document.createElement('div')
+    product.className = 'product'
+    shoppingCart.appendChild(product)
+
+    // if (shoppingCart.hasChildNodes()) {
+    //     while (shoppingCart.childNodes.length >= 3) {
+    //         shoppingCart.removeChild(shoppingCart.lastChild)
+    //     }
+    // }
 
 
     let imgDiv = document.createElement('div')
     console.log(imgDiv);
     imgDiv.className = 'product-image'
+
     product.appendChild(imgDiv)
 
     imgDiv.appendChild(image);
