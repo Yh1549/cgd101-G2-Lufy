d8dcf825d56f3fd4bb43be6245636cbabfc128d1
54Cheryl <cherylhsu246@gmail.com>
2022-06-05 20:10:08 +0800

    20220605 cheryl 推

--- src/js/backendDesignerLightBox.js
+++ src/js/backendDesignerLightBox.js
@@ -2,16 +2,24 @@
 function A_showLightBox(){
     document.getElementById("A_lightBox").style.display = "block";
 }
+function B_showLightBox(){
+    document.getElementById("B_lightBox").style.display = "block";
+}
 //-------------------
 function A_cancelNew(){
     document.getElementById("A_lightBox").style.display = "none";
+}
+function B_cancelNew(){
+    document.getElementById("B_lightBox").style.display = "none";
 }
 //-------------------
 function init(){
     //-------------------showLightbox
     document.getElementById("A_Edit").onclick = A_showLightBox;
+    document.getElementById("B_Edit").onclick = B_showLightBox;
     //-------------------cancelLightbox
     document.getElementById("A_btnCancel").onclick = A_cancelNew;
+    document.getElementById("B_btnCancel").onclick = B_cancelNew;
 }
 
 window.addEventListener("load",init,false);
