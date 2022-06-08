942531f1663fe0dfc668c58d3b2baf5a79be7c9c
Yh1549 <cpeter618@gmail.com>
2022-06-08 23:23:21 +0800

    0608 憶洪 1.註冊功能第一階段完成 缺乏重複註冊的驗證(目前只有顯示 沒有檔下來) 輸入資料正確與否的驗證

--- src/js/memberRegister.js
+++ src/js/memberRegister.js
@@ -1,5 +1,5 @@
 let registerCheck = function(e) {
-    console.log(e.target.name);
+    // Name跟email檢查有無重複
     let xhr = new XMLHttpRequest();
     xhr.onload = () => {
         if (xhr.status == 200) {
@@ -8,25 +8,45 @@ let registerCheck = function(e) {
             alert(xhr.status);
         }
     }
-    console.log(e.target.name);
     xhr.open("post", "memberRegister.php", true);
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    // let data_info = `${e.target.name}=` + document.getElementById(`${e.target.name}`).value;
-    let data_info = {
-
-    };
-    console.log(e.target.name);
+    let data = {};
+    data[e.target.name] = document.getElementById(e.target.name).value;
+    let data_info = `json=${JSON.stringify(data)}`;
     xhr.send(data_info);
+    console.log(data_info);
 };
 
-
+let register = function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            document.getElementById('idMsg').innerText = xhr.responseText;
+        } else {
+            alert(xhr.status);
+        }
+    }
+    xhr.open("post", "memberRegister.php", true);
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    let dataReg = {};
+    dataReg.memName = document.getElementById("memName").value;
+    dataReg.email = document.getElementById("email").value;
+    dataReg.memPsw = document.getElementById("memPsw").value;
+    dataReg.memBD = document.getElementById("memBD").value;
+    dataReg.memPhone = document.getElementById("memPhone").value;
+    dataReg.Address = document.getElementById("Address").value;
+    let data_info = `json=${JSON.stringify(dataReg)}`;
+    xhr.send(data_info);
+    console.log(data_info)
+}
+//初始化
 let init = () => {
+// ----Name跟email檢查
     let ip = document.querySelectorAll(".regInputCheck");
     for (let i = 0; i < ip.length; i++) {
         ip[i].onchange = registerCheck;
-        // console.log(ip[0]); 
-    }
-    //    console.log(ip[0]); 
+    } 
+// ----註冊按鈕
+    document.getElementById("ipReg").addEventListener("click", register, false);
 }
-
 window.addEventListener("load", init, false);
--- src/memberRegister.php
+++ src/memberRegister.php
@@ -1,28 +1,70 @@
 <?php
-try{
+$json = $_POST["json"];
+$data = json_decode($json, true);
+$jsonCount = count($data);
+if (array_key_first($data) == 'memName' && $jsonCount == 1) {
+  try {
+        // Name跟email檢查有無重複
     require_once("connect_lufy.php");
-
-    // $memName = $_POST["memName"];
-    // $email = $_POST["email"];
-    // $memBD = $_POST["memBD"];
-    // $memPhone = $_POST["memPhone"];
-    // $Address = $_POST["Address"];
-
-
-    $sql = "select * from member where member_mail=:email" ;
-    
+    $sql = "select * from member where member_name=:memName";
     $member = $pdo->prepare($sql);
-    $member->bindValue(":email", $_POST["email"]);
-    // $member->bindValue(":memPsw", $_POST["memPsw"]);
+    $member->bindValue(":memName", $data["memName"]);
     $member->execute();
-    if( $member->rowCount() != 0){
-        echo "This email has already been registered";
-        // $memRow = $member->fetch(PDO::FETCH_ASSOC);//接住資料庫成為陣列
-        //登入成功,將登入者的資料寫入cookie
-      }else{
-        echo "This email okok";
-      } 
-}catch(PDOException $e){
+    if ($member->rowCount() != 0) {
+      echo "This Name has already been registered";
+    } else {
+      echo "This name okok";
+    }
+  } catch (PDOException $e) {
     echo "error";
+  }
+}else if (array_key_first($data) == 'email' && $jsonCount == 1) {
+  try {
+        // Name跟email檢查有無重複
+    require_once("connect_lufy.php");
+    $sql = "select * from member where member_mail=:email";
+    $member = $pdo->prepare($sql);
+    $member->bindValue(":email", $data["email"]);
+    $member->execute();
+    if ($member->rowCount() != 0) {
+      echo "This email has already been registered";
+    } else {
+      echo "This email okok";
+    }
+  } catch (PDOException $e) {
+    echo "error";
+  }
+}else if($jsonCount > 1){
+  try {
+    require_once("connect_lufy.php");
+    $sql = "insert into member(member_name, member_mail, member_psw, member_birthday, member_tel, member_address, member_pic, credit_card, card_date, card_checkCode) values(:memName, :email, :memPsw, :memBD, :memPhone, :Address, '', '', '', '')";
+    $member = $pdo->prepare($sql);
+    $member->bindValue(":memName", $data["memName"]);
+    $member->bindValue(":email", $data["email"]);
+    $member->bindValue(":memPsw", $data["memPsw"]);
+    $member->bindValue(":memBD", $data["memBD"]);
+    $member->bindValue(":memPhone", $data["memPhone"]);
+    $member->bindValue(":Address", $data["Address"]);
+    $member->execute();
+    if ($member->rowCount() != 0) {
+      echo "This email has already been registered";
+      // $memRow = $member->fetch(PDO::FETCH_ASSOC);//接住資料庫成為陣列
+    } else {
+      echo "This email okok";
+    }
+  } catch (PDOException $e) {
+    // echo "error";
+    echo $e->getMessage();
+  }
+
+
+
+
+
+
+
+
+
+
 }
 ?>
