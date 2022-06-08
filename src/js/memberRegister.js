b57597bc6bf60061cc5f3aa6012732f1e2d1389a
Yh1549 <cpeter618@gmail.com>
2022-06-08 17:28:34 +0800

    0608 憶洪 1.小修改

--- src/js/memberRegister.js
+++ src/js/memberRegister.js
@@ -1,26 +1,32 @@
-let registerCheck = function(e){
+let registerCheck = function(e) {
+    console.log(e.target.name);
     let xhr = new XMLHttpRequest();
     xhr.onload = () => {
         if (xhr.status == 200) {
-          document.getElementById('idMsg').innerText = xhr.responseText;
+            document.getElementById('idMsg').innerText = xhr.responseText;
         } else {
-          alert(xhr.status);
+            alert(xhr.status);
         }
     }
+    console.log(e.target.name);
     xhr.open("post", "memberRegister.php", true);
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    let data_info = `${e.target.name}=` + document.getElementById(`${e.target.name}`).value;
+    // let data_info = `${e.target.name}=` + document.getElementById(`${e.target.name}`).value;
+    let data_info = {
+
+    };
+    console.log(e.target.name);
     xhr.send(data_info);
 };
 
 
-let init=()=>{
-   let ip = document.querySelectorAll(".regInput");
-   for(let i=0; i<ip.length; i++){
-    ip[i].onchange = registerCheck;
-    // console.log(ip[0]); 
-   }
-//    console.log(ip[0]); 
+let init = () => {
+    let ip = document.querySelectorAll(".regInputCheck");
+    for (let i = 0; i < ip.length; i++) {
+        ip[i].onchange = registerCheck;
+        // console.log(ip[0]); 
+    }
+    //    console.log(ip[0]); 
 }
 
 window.addEventListener("load", init, false);
--- src/memberRegister.php
+++ src/memberRegister.php
@@ -2,19 +2,25 @@
 try{
     require_once("connect_lufy.php");
 
-    $sql = "select * from member where member_mail=:memId and member_psw=:memPsw" ;
+    // $memName = $_POST["memName"];
+    // $email = $_POST["email"];
+    // $memBD = $_POST["memBD"];
+    // $memPhone = $_POST["memPhone"];
+    // $Address = $_POST["Address"];
+
+
+    $sql = "select * from member where member_mail=:email" ;
     
     $member = $pdo->prepare($sql);
-    $member->bindValue(":memId", $_POST["memId"]);
-    $member->bindValue(":memPsw", $_POST["memPsw"]);
+    $member->bindValue(":email", $_POST["email"]);
+    // $member->bindValue(":memPsw", $_POST["memPsw"]);
     $member->execute();
     if( $member->rowCount() != 0){
-        echo "登入成功";
-        $memRow = $member->fetch(PDO::FETCH_ASSOC);//接住資料庫成為陣列
+        echo "This email has already been registered";
+        // $memRow = $member->fetch(PDO::FETCH_ASSOC);//接住資料庫成為陣列
         //登入成功,將登入者的資料寫入cookie
-     
       }else{
-        echo "輸入錯誤";
+        echo "This email okok";
       } 
 }catch(PDOException $e){
     echo "error";
