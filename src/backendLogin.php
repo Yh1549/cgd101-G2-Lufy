58dfa8040cdbd8b9ed99253206cddca793df0f28
Yh1549 <cpeter618@gmail.com>
2022-06-16 23:50:33 +0800

    0616 憶洪 1.後台登入第一階段完成 已可以登入 2.我感覺設計師後台會搞死人

---
+++ src/backendLogin.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+try {
+    require_once("connect_lufy.php");
+    $adminsql = "select * from manager where account=:adminId and password=:adminPsw ";//登入
+  $admin = $pdo->prepare($adminsql);
+  $admin->bindValue(":adminId", $_POST["adminId"]);
+  $admin->bindValue(":adminPsw", $_POST["adminPsw"]);
+  $admin->execute();
+  if ($admin->rowCount() != 0) {
+    $adminRow = $admin->fetch(PDO::FETCH_ASSOC);
+    if($adminRow["state"] != 1){
+    $_SESSION["manager_no"] = $adminRow["manager_no"];
+    $_SESSION["account"] = $adminRow["account"];
+    $_SESSION["manager_name"] = $adminRow["manager_name"];
+    $_SESSION["role"] = $adminRow["role"];
+        if($adminRow["role"] == "後台管理人員"){
+            echo "backend.html";
+        }else if($adminRow["role"] == "設計師"){
+            echo "backendDesigner.html";
+        }
+    }else{
+        echo "此帳號已凍結";
+    }
+  }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
