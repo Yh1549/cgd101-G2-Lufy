7221ed4f9347e5b6fb3d0ffdf44d8b2268561cf6
54Cheryl <cherylhsu246@gmail.com>
2022-06-17 17:28:55 +0800

    20220617 cheryl 修好最新消息

--- src/js/backendLightBox.js
+++ src/js/backendLightBox.js
@@ -23,7 +23,7 @@ function A_create(){//----新增
         alert(xhr.responseText);
         location.href="getProducts.php";
     }
-    xhr.open("post", "productsInsert.php");
+    xhr.open("post", "backend_productsInsert.php");
     xhr.send(new FormData($id("productssForm")));
 }
 //----------------------------
@@ -46,10 +46,10 @@ function B_create(){//----新增
     let xhr = new XMLHttpRequest();
     xhr.onload = function(){
         alert(xhr.responseText);
-        location.href="getPromotions.php";
     };
-    xhr.open("post", "promotionsInsert.php");
+    xhr.open("post", "backend_promotionsInsert.php");
     xhr.send(new FormData($id("promotionsForm")));
+    $id("B_lightBox").style.display = "none";
 }
 //----------------------------
 function B_showEditLightBox() {//-----------開修改燈箱
@@ -71,10 +71,11 @@ function C_create() {//----新增
     let xhr = new XMLHttpRequest();
     xhr.onload = function () {
         alert(xhr.responseText);
-        location.href = "getNews.php";
+        //location.href = "getNews.php";
     };
-    xhr.open("post", "newsInsert.php");
+    xhr.open("post", "backend_newsInsert.php");
     xhr.send(new FormData($id("newsForm")));
+    $id("C_lightBox").style.display = "none";
 };
 //----------------------------
 function C_showEditLightBox() {//-----------開修改燈箱
@@ -104,6 +105,39 @@ function D_cancelEdit() {//-----------取消開修改燈箱
 
 //-----------------------init-------------------------------------------------------------
 function init() {
+    //-------------------新增 C_最新消息按鈕--------------------
+    $id("C_New").onclick = C_showLightBox;//----新增最新消息-New鈕
+    $id("C_btnAdd").onclick = C_create;//----新增最新消息-Confirm鈕
+    $id("C_btnCancel").onclick = C_cancelNew;//----新增最新消息-取消鈕
+    $id("C_upFile").onchange = function (e) {//----新增最新消息上傳圖片鈕
+        let file = e.target.files[0]; //取得所選的file物件的參考
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $id("C_newsImage").src = reader.result;
+        };
+        reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
+    };
+    //-------------------修改 C_最新消息按鈕--------------------
+    let CdEts = document.querySelectorAll("#C_Edit");//----列表的修改最新消息Edit鈕
+    for (let i = 0; i < CdEts.length; i++) {
+        CdEts[i].onclick = C_showEditLightBox;
+    };
+    //----//----修改最新消息-Edit鈕
+    $id("C_EditBtnCancel").onclick = C_cancelEdit;//----修改最新消息-取消鈕
+    $id("C_E_upFile").onchange = function (e) {//----新增最新消息上傳圖片鈕
+        let file = e.target.files[0]; //取得所選的file物件的參考
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $id("C_E_newsImage").src = reader.result;
+        };
+        reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
+    };
+    
+    //-------------------showLightbox
+    // $id("D_New").onclick = D_showLightBox;
+    // //-------------------cancelLightbox
+    // $id("D_btnCancel").onclick = D_cancelNew;
+
     /*
     //-------------------新增 A_商品按鈕--------------------
     $id("A_New").onclick = A_showLightBox;//----新增最新消息-New鈕
@@ -148,6 +182,7 @@ function init() {
         };
         reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
     };
+    */
     //-------------------新增 B_優惠活動按鈕--------------------
     $id("B_New").onclick = B_showLightBox;//----新增優惠活動-New鈕
     $id("B_btnAdd").onclick = B_create;//----新增優惠活動-Confirm鈕
@@ -156,7 +191,7 @@ function init() {
         let file = e.target.files[0]; //取得所選的file物件的參考
         let reader = new FileReader();
         reader.onload = function (e) {
-            $id("B_newsImage").src = reader.result;
+            $id("B_promotionsImage").src = reader.result;
         };
         reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
     };
@@ -171,47 +206,11 @@ function init() {
         let file = e.target.files[0]; //取得所選的file物件的參考
         let reader = new FileReader();
         reader.onload = function (e) {
-            $id("B_E_newsImage").src = reader.result;
+            $id("B_E_promotionsImage").src = reader.result;
         };
         reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
     };
-    */
     //-------------------new_promotionsProduct
-    
-
-
-    //-------------------新增 C_最新消息按鈕--------------------
-    $id("C_New").onclick = C_showLightBox;//----新增最新消息-New鈕
-    $id("C_btnAdd").onclick = C_create;//----新增最新消息-Confirm鈕
-    $id("C_btnCancel").onclick = C_cancelNew;//----新增最新消息-取消鈕
-    $id("C_upFile").onchange = function (e) {//----新增最新消息上傳圖片鈕
-        let file = e.target.files[0]; //取得所選的file物件的參考
-        let reader = new FileReader();
-        reader.onload = function (e) {
-            $id("C_newsImage").src = reader.result;
-        };
-        reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
-    };
-    //-------------------修改 C_最新消息按鈕--------------------
-    let CdEts = document.querySelectorAll("#C_Edit");//----列表的修改最新消息Edit鈕
-    for (let i = 0; i < CdEts.length; i++) {
-        CdEts[i].onclick = C_showEditLightBox;
-    };
-    //----//----修改最新消息-Edit鈕
-    $id("C_EditBtnCancel").onclick = C_cancelEdit;//----修改最新消息-取消鈕
-    $id("C_E_upFile").onchange = function (e) {//----新增最新消息上傳圖片鈕
-        let file = e.target.files[0]; //取得所選的file物件的參考
-        let reader = new FileReader();
-        reader.onload = function (e) {
-            $id("C_E_newsImage").src = reader.result;
-        };
-        reader.readAsDataURL(file); //將圖檔資訊讀出以 base64的編碼方式，以便放到img標籤中
-    };
-
-    //-------------------showLightbox
-    // $id("D_New").onclick = D_showLightBox;
-    // //-------------------cancelLightbox
-    // $id("D_btnCancel").onclick = D_cancelNew;
 };
 
 window.addEventListener("load", init, false);
