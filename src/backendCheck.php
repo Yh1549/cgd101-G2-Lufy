91d18216e1be4baeec9a2b21720f9ab4fa22ddfd
Yh1549 <cpeter618@gmail.com>
2022-06-17 21:24:09 +0800

    0617 憶洪 1.commit一下 沒有甚麼重要的

---
+++ src/backendCheck.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+try {
+    require_once("connect_lufy.php");
+if(isset($_SESSION["role"])){
+if ($_SESSION["role"] == "後台管理人員") {
+        echo "admin";
+    } else if ($_SESSION["role"] == "設計師") {
+        echo "fail";
+    }
+}else{
+    echo "fail";
+}
+    
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
