bcc3f4a36824fa001915e9a89f988d13065f094a
Yh1549 <cpeter618@gmail.com>
2022-06-22 17:49:05 +0800

    0622

--- src/connect_lufy.php
+++ src/connect_lufy.php
@@ -1,7 +1,7 @@
 <?php
     $dbname= "lufy";
     $user = "root";
-    $password="\$Linz791012";
+    $password="fatjiji0975";
 
     $dsn="mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
