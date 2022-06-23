7f6616f3601058eb79960330fbf433dbac687a85
54Cheryl <cherylhsu246@gmail.com>
2022-06-23 17:16:06 +0800

    no message

--- src/connect_lufy.php
+++ src/connect_lufy.php
@@ -1,11 +1,7 @@
 <?php
     $dbname= "lufy";
     $user = "root";
-<<<<<<< HEAD
     $password="mike4358";
-=======
-    $password="\$Linz791012";
->>>>>>> 495695b36cda7ec1589f6317fa320da3444bd80d
 
     $dsn="mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
