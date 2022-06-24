5344c7ba2fb2d5c79e7959d8ba74ecdc44a1a8d6
54Cheryl <cherylhsu246@gmail.com>
2022-06-24 11:11:55 +0800

    20220624 cheryl 真的最後的波紋了

--- src/backendCheck.php
+++ src/backendCheck.php
@@ -2,17 +2,21 @@
 session_start();
 try {
     require_once("connect_lufy.php");
-    if ($_SESSION["state"] == 0) {
-        if (isset($_SESSION["role"])) {
-            if ($_SESSION["role"] == "後台管理人員") {
-                echo "admin";
-            } else if ($_SESSION["role"] == "設計師") {
+    if (isset($_SESSION["state"]) == true) {
+        if ($_SESSION["state"] == 0) {
+            if (isset($_SESSION["role"])) {
+                if ($_SESSION["role"] == "後台管理人員") {
+                    echo "admin";
+                } else if ($_SESSION["role"] == "設計師") {
+                    echo "fail";
+                }
+            } else {
                 echo "fail";
             }
         } else {
             echo "fail";
         }
-    }else{
+    } else {
         echo "fail";
     }
 } catch (PDOException $e) {
