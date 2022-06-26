4a20ba2dbca0bedf56e9297a0d3b380848cd02e8
Yh1549 <cpeter618@gmail.com>
2022-06-26 12:30:56 +0800

    會員中心功能新增與修正 註冊名稱檢查刪除 密碼檢查新增 註冊完直接跳轉profile頁 會員中心登入後跳轉profile頁 新增訂單與蒐藏若無項目會出現提醒 新增密碼修改檢查

---
+++ src/oldPswCheck.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+try{
+    require_once("connect_cgd101g2.php");
+        $pswChecksql = "select member_psw from member member_no=:curId";
+        $pswCheck = $pdo->prepare($pswChecksql);
+        $pswCheck ->bindValue(":curId", $_SESSION["memNo"]);
+        $pswCheck ->execute();
+        $pswCheckRow = $pswCheck->fetch(PDO::FETCH_ASSOC);
+    if($_POST["pswC"] == $pswCheckRow["member_psw"]){
+        echo "ok";
+    }else{
+        echo "no";
+    }
+}catch(PDOException $e) {
+    // echo "error";
+    echo $e->getMessage();
+  }
+?>
